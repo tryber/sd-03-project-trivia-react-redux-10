aa516ce4a901afe027c17449eebecc0f0d459d47
felipedoss <felipecaetano25.1996@gmail.com>
2020-06-08 17:21:14 -0300

    starting project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           SUA VEZ
         </p>
