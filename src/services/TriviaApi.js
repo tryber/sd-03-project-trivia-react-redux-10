513860d7168e91ed3497343701d0e753fc458a06
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-06-08 18:15:25 -0300

    first commit

---
+++ src/services/TriviaApi.js
@@ -0,0 +1,12 @@
+const TOKEN_URL = 'https://opentdb.com/api_token.php?command=request';
+
+const getToken = () => fetch(TOKEN_URL)
+  .then((response) => response.json()
+  .then((json) => (response.response_code === 0 ? Promise.resolve(json) : Promise.reject(json))));
+
+
+
+
+const BASE_URL = 'https://opentdb.com/api.php?amount=5';
+
+export default getToken;
