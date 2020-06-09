5f14b455b1880029ac7c85d4d1425630c859535f
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-06-09 16:58:43 -0300

    Adjusting errros indicated by CodeClimate

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './PlayerCard.css';
 
 class PlayerCard extends React.Component {
@@ -6,12 +7,17 @@ class PlayerCard extends React.Component {
     const { player, index } = this.props;
     return (
       <div className="player-div">
-        <img src={player.picture} className="image" />
+        <img src={player.picture} className="image" alt={player.name}/>
         <p data-testid={`player-name-${index}`} className="name">{player.name}</p>
         <p data-testid={`player-score-${index}`} className="score">{`${player.score} pontos`}</p>
       </div>
-    )
+    );
   }
+}
+
+PlayerCard.propTypes = {
+  player: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 }
 
 export default PlayerCard;
